28c264226f43597e0c7fbf2dcb63d9d520539032
Morgan J Lopes <morganjlopes@gmail.com>
2015-11-09 21:03:51 -0500

    Create Users with Devise.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :features
 
   resources :vehicles
@@ -7,6 +8,7 @@ Rails.application.routes.draw do
 
   resources :makes
 
+  root to: "vehicles#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
