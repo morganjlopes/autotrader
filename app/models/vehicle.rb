072888b66cea60c07a68e6ef5c8751c689a32861
Morgan J Lopes <morganjlopes@gmail.com>
2015-11-16 19:36:26 -0500

    Add users to vehicles.

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,5 +1,7 @@
 class Vehicle < ActiveRecord::Base
   belongs_to :category
+  belongs_to :user
+  
   has_many :vehicle_features
   has_many :features, through: :vehicle_features
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151110012711) do
+ActiveRecord::Schema.define(version: 20151116233315) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 20151110012711) do
     t.float    "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
 end
