ebaea19f1dab7ce473e51834086a39f715f2f18f
Morgan J Lopes <morganjlopes@gmail.com>
2015-11-16 20:07:36 -0500

    Install Friendly_id and add slugs to vehicles.

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,7 +1,20 @@
 class Vehicle < ActiveRecord::Base
+  extend FriendlyId
+  friendly_id :sluggable, use: [:slugged, :finders]
+
   belongs_to :category
   belongs_to :user
   
   has_many :vehicle_features
   has_many :features, through: :vehicle_features
+
+  validates_presence_of :category
+
+  def name
+    category.make.name + " " + category.name
+  end
+
+  def sluggable
+    name + " " + Time.now.to_i.to_s
+  end
 end
