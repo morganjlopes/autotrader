19cdc57d2fa541993852a602a86a7c1da7e8d88a
Morgan J Lopes <morganjlopes@gmail.com>
2015-11-16 20:18:41 -0500

    Add slug to makes, categories, and features.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ActiveRecord::Base
+  extend FriendlyId
+  friendly_id :name, use: [:slugged, :finders]
+  
   belongs_to :make 
   validates_presence_of :name, :make 
   validates_uniqueness_of :name
